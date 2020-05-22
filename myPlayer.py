3652f7b2709dd4e18457408dd547daea73a2efba
Maelle Toy-Riont <mtoyriontle@enseirb-matmeca.fr>
2020-05-22 23:32:19 +0200

    IA qui joue avec alpha-beta

--- myPlayer.py
+++ myPlayer.py
@@ -26,6 +26,12 @@ class myPlayer(PlayerInterface):
         return "Random Player"
 
     def getPlayerMove(self):
+
+        max = -123456
+        alpha = -1234567
+        beta = +1234567
+        depth = 3
+
         if self._board.is_game_over():
             print("Referee told me to play but the game is over!")
             res = self._board.result()
@@ -36,22 +42,20 @@ class myPlayer(PlayerInterface):
             else:
                 print("equal")
             return "PASS"
-
-        max = -123456
-        alpha = -1234567
-        beta = +1234567
-        depth = 3
+        
+        best_move = 0
 
         for move in self._board.legal_moves():
             self._board.push(move)
-            print("I am playing ", self._board.move_to_str(move))
-            print("My current board :")
-            self._board.prettyPrint()
             val = self.alphabeta(alpha, beta, False, depth-1)
-            # self._board.pop()
+            self._board.pop()
             if val > alpha:
                 alpha = val
                 best_move = move
+
+            print("BEST MOVE " + Goban.Board.flat_to_name(best_move))
+        
+        self._board.push(best_move)
         return Goban.Board.flat_to_name(best_move)
 
     def alphabeta(self, alpha, beta, maximizePlayer, depth):
@@ -63,12 +67,13 @@ class myPlayer(PlayerInterface):
             if res == "0-1":
                 return 4000000
             else:
-                return 0
+                return 4000000
         # AMI
         if maximizePlayer:
             for move in self._board.legal_moves():
                 self._board.push(move)
-                alpha = max(alpha, self.alphabeta(alpha, beta, False, depth - 1))
+                alpha = max(alpha, self.alphabeta(
+                    alpha, beta, False, depth - 1))
                 self._board.pop()
                 if alpha >= beta:
                     return beta
